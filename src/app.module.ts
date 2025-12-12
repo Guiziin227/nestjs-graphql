c3909d9294f4851141b0a97b5ccccb67d389c27d
Guiziin227 <gh63388@gmail.com>
2025-12-12 19:10:04 -0300

    Configuração do typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import dbConfig from './config/db.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env`,
+      load: [dbConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: dbConfig,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
