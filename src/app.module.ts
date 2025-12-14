2be0772640fa0e1637d631144424acbce7da7b7f
Guiziin227 <gh63388@gmail.com>
2025-12-14 14:24:50 -0300

    app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { AuthorModule } from './author/author.module';
+import { PostModule } from './post/post.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: true,
+      autoSchemaFile: './schema.gql',
       debug: true,
       playground: true,
     }),
+    AuthorModule,
+    PostModule,
   ],
   controllers: [AppController],
   providers: [AppService],
