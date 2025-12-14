1604db573a6df1b219f521427730f16e2beca4bb
Guiziin227 <gh63388@gmail.com>
2025-12-14 13:50:41 -0300

    melhorando

--- src/author/author.resolver.ts
+++ src/author/author.resolver.ts
@@ -9,6 +9,7 @@ import {
 import { AuthorService } from './author.service';
 import { PostService } from 'src/post/post.service';
 import { Author } from './models/author.model';
+import { Post } from 'src/post/models/post.model';
 
 @Resolver()
 export class AuthorResolver {
@@ -17,13 +18,13 @@ export class AuthorResolver {
     private readonly postService: PostService,
   ) {}
 
-  @Query(() => Author)
-  author(@Args('id', { type: () => Int }) id: number) {
+  @Query(() => Author, { name: 'author' })
+  getAuthor(@Args('id', { type: () => Int }) id: number) {
     return this.authorService.findOneById(id);
   }
 
-  @ResolveField()
-  posts(@Parent() autor: Author) {
+  @ResolveField('posts', () => [Post])
+  getPosts(@Parent() autor: Author) {
     const { id } = autor;
     return this.postService.findPostsByAuthorId(id);
   }
